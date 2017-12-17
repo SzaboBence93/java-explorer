a997a44e969b102fa3bf3073c16384a0e410ef18
Szabo Bence <szabobence01@gmail.com>
2017-12-17 17:48:08 +0100

    anything

--- src/hu/explore/Main.java
+++ src/hu/explore/Main.java
@@ -9,7 +9,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-
         int neszeTe = IntStream.range(1,10)
                 .filter(i -> i%2 == 0)
                 .map(i -> i*i)

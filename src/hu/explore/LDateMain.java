b4c842dce1183c01240213f06d068889d568a136
Szabo Bence <szabobence01@gmail.com>
2017-12-17 18:08:34 +0100

    anything

--- src/hu/explore/LDateMain.java
+++ src/hu/explore/LDateMain.java
@@ -10,7 +10,6 @@ import java.time.format.DateTimeFormatter;
 public class LDateMain {
 
     public static void main(String[] args) {
-
         LocalDate localDate = LocalDate.of(2004, 2, 29);
         LocalDateTime ect = LocalDateTime.now(ZoneId.of("Europe/Budapest"));
         LocalDateTime utc = LocalDateTime.now(Clock.systemUTC());
